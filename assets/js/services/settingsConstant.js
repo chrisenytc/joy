25d59a9589f23cab8ff86bf729f6269b3ac1e939
Gabriel Pedro <gabriel@rcfinovacoes.com.br>
2014-10-07 16:44:22 -0400

    Move Recaptcha Key to SettingsContant.js

--- assets/js/services/settingsConstant.js
+++ assets/js/services/settingsConstant.js
@@ -1,5 +1,6 @@
 'use strict';
 
 app.constant('$settings', {
-    apiUri: 'http://localhost:1337'
+    apiUri: 'http://localhost:1337',
+    recaptcha: '6LeYhvMSAAAAALKpLXVHgiM_W_WovgVaY4DnxoIK'
 });
